a514bd6481b02a535487fdc13b06aec96c414e41
psmarcin <ps@psmarcin.me>
2019-03-07 19:01:20 +0100

    feat: front page ui improvements

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app" class="uk-scope uk-container">
     <div class="uk-card uk-card-default uk-card-body uk-card-hover">
+      <div class="logotype"/>
       <h3 class="uk-card-title">Youtube goes PODCAST!</h3>
+      <p
+        uk-tooltip="title: Hello World"
+      >Let us generate podcasts feed from your favourite YouTube channels.</p>
       <Search/>
       <ChannelList/>
       <Link/>
@@ -23,3 +27,16 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style>
+.logotype {
+  width: 100%;
+  height: 100px;
+  background: url("https://www.9marks.org/wp-content/uploads/2012/08/Podcast.jpg")
+    #e4701d center center repeat-x;
+  background-size: contain;
+}
+.uk-card-title {
+  text-align: center;
+}
+</style>
